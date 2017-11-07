4c55356dd08ffd7fde902ab32a2256a88879bba2
Jennifer Rhees <redswinglinestapler23@gmail.com>
2017-11-07 04:14:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,3 +11,4 @@ const newID = oldID + 1000000000;
 ageIsValid = Number.isInteger(currentAge);
 let randomNumber = Math.random() * 20;
 let randomInteger = Math.floor(randomNumber);
+let randomUserID = randomInteger + 1000000000;
